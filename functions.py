dbd60ba947b348c1f153fdc473f613384a455bff
Jull6 <mamamia112004@gmail.com>
2025-02-25 15:05:24 +0300

    add date and reactions to log

--- functions.py
+++ functions.py
@@ -1,2 +1,10 @@
 def extract_features(message):
     return [1, 2, 3]
+
+def extract_reactions(message):
+    result = {}
+    if message.reactions is not None: 
+        for reaction in message.reactions.reactions:
+            if not reaction.emoji in result:
+                result[reaction.emoji] = reaction.count
+    return result
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 from credentials import api_id, api_hash
-from functions import extract_features
+from functions import extract_features, extract_reactions
 import logging
 import json
 
@@ -8,14 +8,12 @@ import json
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w", encoding="utf-8")
 
 # Канал для считывания информации
-channel_username = '@rian_ru'
+channel_username = '@befree_community'
 
 app = Client("enotta", api_id, api_hash)
 with app:
-    messages = app.get_chat_history(channel_username, limit=5)
+    messages = app.get_chat_history(channel_username, limit=20)
     for message in messages:
-        if message.text is None:
-            logging.warning("Пост без текстового описания!")
-        else:
-            post = {"text": message.text, "tags": extract_features(message), "views": message.views}
-            logging.info(msg=json.dumps(post, ensure_ascii=False))
+        print(app.get_discussion_message(channel_username, message.id))
+        post = {"text": message.text, "tags": extract_features(message), "views": message.views, "date": str(message.date), "reactions":extract_reactions(message)}
+        logging.info(msg=json.dumps(post, ensure_ascii=False))
