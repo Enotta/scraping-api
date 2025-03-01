596ff0671204920ee3d9fcb5eb874282f8cb3990
Enotta <nvageev_1@edu.hse.ru>
2025-03-01 17:23:16 +0300

    several channels parsed & ipynd data preparation

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ import time
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w", encoding="utf-8")
 
 # Канал для считывания информации
-channel_username = '@novosti_voinaa'
-# Novosty
+channel_usernames = [
+    '@novosti_voinaa',
+    '@Novosty',
+    '@topor',
+    '@moscowmap',
+    '@moscowach'
+]
 
 # Ключевые слова для блока
 PARTICIPATE_KEYWORDS = ["участвовать", "participate", "розыгрыш", "giveaway"]
@@ -20,51 +25,52 @@ PARTICIPATE_KEYWORDS = ["участвовать", "participate", "розыгры
 app = Client("enotta", api_id, api_hash)
 async def main():
     async with app:
-        async for message in app.get_chat_history(channel_username, limit=50):
-            try:
-                # Дроп постов с переходом
-                if message.reply_markup:
-                    buttons = False
-                    for row in message.reply_markup.inline_keyboard:
-                        if len(row) > 0:
-                            buttons = True
-                            break
-
-                    if buttons:
-                        logging.warning("Пост с рекламой!")
-                        continue
-                
-                # Выбор текста в зависимости от медиа
-                text = ""
-                if message.media is not None:
-                    text = message.caption
-                else:
-                    text = message.text
-
-                # Подсчет комментариев
-                comments = None
+        for channel_username in channel_usernames:
+            async for message in app.get_chat_history(channel_username, limit=2000):
                 try:
-                    if message.media_group_id is None:
-                        comments = await app.get_discussion_replies_count(channel_username, message.id)
+                    # Дроп постов с переходом
+                    if message.reply_markup:
+                        buttons = False
+                        for row in message.reply_markup.inline_keyboard:
+                            if len(row) > 0:
+                                buttons = True
+                                break
+
+                        if buttons:
+                            logging.warning("Пост с рекламой!")
+                            continue
+                    
+                    # Выбор текста в зависимости от медиа
+                    text = ""
+                    if message.media is not None:
+                        text = message.caption
                     else:
-                        group = await message.get_media_group()
-                        if message.id == group[0].id:
+                        text = message.text
+
+                    # Подсчет комментариев
+                    comments = None
+                    try:
+                        if message.media_group_id is None:
                             comments = await app.get_discussion_replies_count(channel_username, message.id)
                         else:
-                            logging.warning("Часть медиа группы!")
-                            continue
-                except pyrogram.errors.exceptions.bad_request_400.MsgIdInvalid as e:
-                    logging.warning("Пост без возможности подсчёта комментариев!")
+                            group = await message.get_media_group()
+                            if message.id == group[0].id:
+                                comments = await app.get_discussion_replies_count(channel_username, message.id)
+                            else:
+                                logging.warning("Часть медиа группы!")
+                                continue
+                    except pyrogram.errors.exceptions.bad_request_400.MsgIdInvalid as e:
+                        logging.warning("Пост без возможности подсчёта комментариев!")
 
-                post = {
-                    "text": text, "views": message.views, "date": str(message.date), 
-                    "tags": extract_features(text),  
-                    "reactions": extract_reactions(message), "comments": comments,
-                    "media": str(message.media)[17:]
-                }
-                logging.info(msg=json.dumps(post, ensure_ascii=False))
-            except pyrogram.errors.exceptions.flood_420.FloodWait as e:
-                # Сон на требуемой количество секунд!
-                await asyncio.sleep(e.value)
+                    post = {
+                        "channel": channel_username,
+                        "text": text, "views": message.views, "date": str(message.date), 
+                        "tags": extract_features(text), "reactions": extract_reactions(message), "comments": comments,
+                        "media": str(message.media)[17:]
+                    }
+                    logging.info(msg=json.dumps(post, ensure_ascii=False))
+                except pyrogram.errors.exceptions.flood_420.FloodWait as e:
+                    # Сон на требуемой количество секунд!
+                    await asyncio.sleep(e.value)
 
 app.run(main())
